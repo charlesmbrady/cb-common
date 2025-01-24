43391433a22bd400cdea81ed2bf1d2794bc32f59
charlesmbrady <charlesmbrady@gmail.com>
2025-01-24 02:41:57 -0500

    add mockdat middleware for nestj.s server

---
+++ apps/mockdat-middleware/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'mockdat-middleware',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/mockdat-middleware',
+};
