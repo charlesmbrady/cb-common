b2a946aaf7f588200b15bd1af09c997c279f63be
charlesmbrady <charlesmbrady@gmail.com>
2025-11-29 02:58:54 -0500

    fix affected scripts not picking up properly bc of nx command not working anymore

--- apps/api-services/src/controllers/api.controller.ts
+++ apps/api-services/src/controllers/api.controller.ts
@@ -7,7 +7,7 @@ export const getInfo = (req: Request, res: Response) => {
   console.log('event', event);
   res.json({
     service: 'API Services',
-    version: '1.0.7+',
+    version: '1.0.8+',
     timestamp: new Date().toISOString(),
   });
 };
