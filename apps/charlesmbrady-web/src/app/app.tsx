3eccc5d9f1a23d22615cb84ea89a4acbe65c5a29
charlesmbrady <charlesmbrady@gmail.com>
2025-12-04 01:43:43 -0500

    remove 'test' from branding in nav

--- apps/charlesmbrady-web/src/app/app.tsx
+++ apps/charlesmbrady-web/src/app/app.tsx
@@ -49,7 +49,7 @@ export default function App() {
               }}
               onClick={() => (window.location.href = '/')}
             >
-              Charles Brady - TEST
+              Charles Brady
             </Box>
           ),
           navigationProps: {
